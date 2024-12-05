e0f14aba4d31f8bd8b9dde18dd39ee6e0bb8e15a
Raphael Vigneault Ð. <triangle1232009@hotmail.com>
2024-12-04 20:05:05 -0500

    CSVHelper

--- src/CSVPlayground/CSVHelper.php
+++ src/CSVPlayground/CSVHelper.php
@@ -13,23 +13,16 @@ use League\Csv\Statement;
 class CSVHelper
 {
     private $filePath;
-    private $csvRows;
     private $data;
 
     public function __construct($filePath)
     {
         $this->filePath = $filePath;
         $this->parseCsvFile();
-        $this->parseCsvRow();
         $this->print();
     }
 
-    /**
-     * Parses the CSV file specified by the file path, converting its encoding to UTF-8 if necessary.
-     * Loads the CSV data using the League\Csv library and processes it into an array of records.
-     * Stores the parsed records in the csvRows property and handles any exceptions that occur during parsing.
-     */
-    private function parseCsvFile(): void
+    private function parseCsvFile(string $delimiter = ";", string $escape = "\\", string $enclosure = '"'): void
     {
         try {
             // Detect the file's encoding
@@ -40,47 +33,35 @@ class CSVHelper
                 $fileContent = mb_convert_encoding($fileContent, 'UTF-8', $encoding);
             }
 
-            // Load the converted content into League\Csv
+            // Transcode the file into a temp file
             $tmpFilename = 'temp_file.csv';
             file_put_contents($tmpFilename, $fileContent);
-            $fileContent = Reader::createFromPath($tmpFilename, 'r');
+
+            // Load the converted content into League\Csv
+            $csv = Reader::createFromPath($tmpFilename, 'r');
+            $csv->setEscape($escape);
+            $csv->setEnclosure($enclosure);
+            $csv->setDelimiter($delimiter);
 
             // If the CSV file has a header at row [0]
-            // $csv->setHeaderOffset(0);
+            // $fileContent->setHeaderOffset(0);
 
             // Parse the CSV data into an array
-            $records = (new Statement())->process($fileContent);
+            $records = (new Statement())->process($csv);
 
             // Convert records to an array
-            $csvRows = [];
+            $data = [];
             foreach ($records as $record) {
-                $csvRows[] = $record;
+                $data[] = $record;
             }
 
             // Clean up temporary file
             unlink($tmpFilename);
 
-            $this->csvRows = $csvRows;
+            $this->data = $data;
         } catch (Exception $e) {
             echo "Error parsing csv file: " . $e->getMessage();
         }
-    }
-
-
-    /**
-     * Copies the parsed CSV rows into the data property.
-     *
-     * @param string $delimiter The delimiter used in the CSV file. Default is ";".
-     * @param string $escaper The escape character used in the CSV file. Default is "\\".
-     */
-    private function parseCsvRow(string $delimiter = ";", string $escaper = "\\")
-    {
-        $data = [];
-        foreach ($this->csvRows as $row) {
-            $data[] = $row;
-        }
-
-        $this->data = $data;
     }
 
     /**
@@ -88,6 +69,6 @@ class CSVHelper
      */
     private function print()
     {
-        print_r($this->data);
+        print_r(json_encode($this->data));
     }
 }
